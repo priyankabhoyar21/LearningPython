41c8710a862d5de850339b0b2887756e1e772095
Priyanka <priyanka.bhoyar@digipen.edu>
2021-12-20 13:26:29 +0800

    Adding Test File

---
+++ Test.py
@@ -0,0 +1,18 @@
+print("Welcome to debugging!")
+
+def add():
+    a = 2
+    b = 0
+    total = a + b
+    return total
+
+result = add()
+print(add())
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
+plt.plot(x, npp.sin(x))       # Plot the sine of each x point
+plt.show() 
